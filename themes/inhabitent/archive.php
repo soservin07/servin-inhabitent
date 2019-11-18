4beff98e0e220f1c5a7c8ff13050f5778518d85d
SERVIN <soservin@ymail.com>
2019-11-17 21:25:24 -0800

    added script and css style
    *worked for my archived page, and added some script to load category details. And list of items.
    *restructured css style

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -11,30 +11,48 @@ get_header(); ?>
 		<main id="main" class="main-site" role="main">
 			<h2>
 		<?php
-					wp_title('');
-					// the_archive_title( '<h1 class="page-title">', '</h1>' );
-					// the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-				</h2>
-		<?php if ( have_posts() ) : ?>
+		$query=get_queried_object();
+		if($query->slug=='do'){
+			echo '<h2>'. strtoupper($query->slug).'</h2>';
+			the_archive_description( '<div class="taxonomy-description">', '</div>' );
+		}
+		else{
+			wp_title('');
+		}
+		
+		// the_archive_title( '<h1 class="page-title">', '</h1>' );
+		
+	
+		?>
+		</h2>
 
+		<section class="article-content">
+
+			<?php 
+			if($query->slug=='do'){
+				$posts=get_posts(array('post_type'=>'product','numberposts'=>-1,'orderby'=>'post_title','tax_query'=>array('taxonomy'=>'products_type',
+				'field'=>'slug','terms'=>'do')));
+			echo '<pre>';
+				print_r($posts);
+			echo '</pre>';
+			}
+			else{
+				if ( have_posts() ) :
+					while ( have_posts() ) : the_post(); 
+		
+						get_template_part( 'template-parts/content' );
+		
+					endwhile; 
+				else : 
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+			}
+			wp_reset_postdata();
 			
-			<section class="article-content">
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
-			<?php endwhile; ?>
+			?>
 
 			<?php //the_posts_navigation(); ?>
 
-			<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; ?>
 			</section>
 			
 
